db51e7143a9bcf4629ef0d52417fc172818ac22c
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-07-25 16:22:36 -0400

    Update Hero.tsx

--- src/app/home/Hero.tsx
+++ src/app/home/Hero.tsx
@@ -8,21 +8,21 @@ export const Hero = () => {
       <FlexboxSpacer maxWidth={75} minWidth={0} className="hidden lg:block" />
       <div className="mx-auto max-w-xl pt-8 text-center lg:mx-0 lg:grow lg:pt-32 lg:text-left">
         <h1 className="text-primary pb-2 text-4xl font-bold lg:text-5xl">
-          Create a professional
+          Professional. Clean.
           <br />
-          resume easily
+          Simple.
         </h1>
         <p className="mt-3 text-lg lg:mt-5 lg:text-xl">
-          With this free, open-source, and powerful resume builder
+          The resume builder tool you've been looking for.
         </p>
         <Link href="/resume-import" className="btn-primary mt-6 lg:mt-14">
-          Create Resume <span aria-hidden="true">→</span>
+          Start Now! <span aria-hidden="true">→</span>
         </Link>
-        <p className="ml-6 mt-3 text-sm text-gray-600">No sign up required</p>
+        <p className="ml-6 mt-3 text-sm text-gray-600">No sign up required (or needed, or wanted).</p>
         <p className="mt-3 text-sm text-gray-600 lg:mt-36">
           Already have a resume? Test its ATS readability with the{" "}
           <Link href="/resume-parser" className="underline underline-offset-2">
-            resume parser
+           our built in resume parser
           </Link>
         </p>
       </div>

fb6d3471867b2c2ed79e5b46885053dfc6f610d3
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-07-25 19:12:07 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { TopNavBar } from "components/TopNavBar";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: "OpenResume - Free Open-source Resume Builder and Parser",
+  title: "Iftiin-Resume - Simple & Powerful Resume Builder and Parser",
   description:
-    "OpenResume is a free, open-source, and powerful resume builder that allows anyone to create a modern professional resume in 3 simple steps. For those who have an existing resume, OpenResume also provides a resume parser to help test and confirm its ATS readability.",
+    "Iftiin-Resume is THE resume builder that allows you to compose a modern professional resume in 3 simple steps. For those who have an existing resume, Iftiin-Resume also provides a built-in resume parser to help test and confirm its ATS readability.",
 };
 
 export default function RootLayout({

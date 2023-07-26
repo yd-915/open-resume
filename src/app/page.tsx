40849d718e7b61f6253341979e4249bd6055bed6
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-07-25 20:23:36 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { QuestionsAndAnswers } from "home/QuestionsAndAnswers";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-black-900 lg:px-12">
+    <main className="mx-auto max-w-screen-2xl bg-dot bg-black-600 px-8 pb-32 text-black-900 lg:px-12">
       <Hero />
       <Steps />
       <Features />

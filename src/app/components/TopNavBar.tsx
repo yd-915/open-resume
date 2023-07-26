2d83a179480a0a968009038d5328c42a9319f6e4
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-07-25 20:34:18 -0400

    Update TopNavBar.tsx

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -19,10 +19,10 @@ export const TopNavBar = () => {
     >
       <div className="flex h-10 w-full items-center justify-between">
         <Link href="/">
-          <span className="sr-only">OpenResume</span>
+          <span className="sr-only">Iftiin-Resume</span>
           <Image
             src={logoSrc}
-            alt="OpenResume Logo"
+            alt="IftiinResume Logo"
             className="h-8 w-full"
             priority
           />
